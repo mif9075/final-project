88e4aa298df593f8d4f9aa6579980e60c8dac1a8
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-11-19 13:21:45 -0500

    Completed App.js in React

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Navbar from './Components/Navbar/Navbar'
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute'
+import Footer from './Components/Footer/Footer'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Login = React.lazy(() => import('./Components/Login/Login'))
+const Register = React.lazy(() => import('./Components/Register/Register'))
+const Home = React.lazy(() => import('./Components/Home/Home'))
+const NotFound = React.lazy(() => import('./Components/NotFound/NotFound'))
+const UserProfile = React.lazy(() => import('./Components/UserProfile/UserProfile'))
+
+class App extends Component {
+
+    render(){
+        return(
+        <>
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/Login" component={Login} />
+                <Route exact path="/Register" component={Register} />
+
+                <PrivateRoute exact path="/user-profile" component={UserProfile} />
+                <Route path="" component={NotFound} />
+            </Switch>
+            <Footer />
+        </>
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        authUser: state.authUser
+    }
+}
+
+export default withRouter(connect(mapStateToProps, null)(App));
