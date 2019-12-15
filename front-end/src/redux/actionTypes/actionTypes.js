8463d748bbadce1bcfedd1aec6392b7ac93dae8c
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-12-15 13:01:39 -0500

    Created Posts Redux

--- front-end/src/redux/actionTypes/actionTypes.js
+++ front-end/src/redux/actionTypes/actionTypes.js
@@ -1,8 +1,12 @@
+//authActions
+
 export const REGISTER_SUCCESS_MESSAGE = 'REGISTER_SUCCESS_MESSAGE';
 export const REGISTER_ERROR_MESSAGE = 'REGISTER_ERROR_MESSAGE';
 
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
 export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS';
+
+// playerReducer/playersReducers
 
 export const GET_ALL_PLAYERS = 'GET_ALL_PLAYERS';
 export const GET_ALL_USER_PLAYERS = 'GET_ALL_USER_PLAYERS';
@@ -12,7 +16,7 @@ export const GET_PLAYER_BY_ID = 'GET_PLAYER_BY_ID';
 export const ERROR_GET_PLAYER = 'ERROR_GET_PLAYER';
 
 
-//searchReducer
+// searchReducer
 
 export const SUBMIT_SEARCH = 'SUBMIT_SEARCH';
 export const ERROR_SEARCH = 'ERROR_SEARCH';
@@ -20,4 +24,18 @@ export const ERROR_SEARCH = 'ERROR_SEARCH';
 export const ALL_PLAYERS = 'ALL_PLAYERS';
 export const ACTIVE_PLAYERS = 'ACTIVE_PLAYERS';
 export const RETIRED_PLAYERS = 'RETIRED_PLAYERS';
+
+// postsActions
+export const CREATE_POST = 'CREATE_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
+export const GET_POSTS = 'GET_POSTS';
+export const LIKE_POST = 'LIKE_POSTS';
+export const UPDATE_POST_LIKES = 'UPDATE_POST_LIKES';
+
+//commentActions
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const GET_COMMENTS = 'GET_COMMENTS'
 
--- front-end/src/redux/reducers/index.js
+++ front-end/src/redux/reducers/index.js
@@ -1,14 +1,17 @@
 import { combineReducers } from 'redux';
+
 import authReducer from './authReducer';
 import handleMessageReducer from './handleMessageReducer'
 import playersReducer from './playersReducer'
 import playerReducer from'./playerReducer'
 import searchReducer from './searchReducer'
+import postsReducer from './postsReducer';
 
 export default combineReducers({
     authUser: authReducer,
     message: handleMessageReducer,
     players: playersReducer,
     player: playerReducer,
-    search: searchReducer
+    search: searchReducer,
+    posts: postsReducer
 });
